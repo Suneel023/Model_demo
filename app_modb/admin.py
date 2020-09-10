e88eac132b70e904e9be4af96f86cd46d89dbe28
Suneel023 <mariochill@yahoo.com>
2020-09-10 14:59:41 +0530

    second commint

--- app_modb/admin.py
+++ app_modb/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from app_modb import models
 
 # Register your models here.
+class TopicAdminView(admin.ModelAdmin):
+    list_display=('topic_name',)
+    search_fields=('topic_name',)
+admin.site.register(models.Topic,TopicAdminView)
+
+class WebpageAdminView(admin.ModelAdmin):
+    list_display=('topic','name','url')
+    search_fields=('topic','name')
+    list_editable=('name',)
+    list_filter=('topic',)
+admin.site.register(models.Webpage,WebpageAdminView)
+
+class AccessdetailsAdminView(admin.ModelAdmin):
+    list_display=('webpage','datetime')
+    search_fields=('webpage','datetime')
+admin.site.register(models.AccessDetail,AccessdetailsAdminView)
--- app_modb/models.py
+++ app_modb/models.py
@@ -4,11 +4,22 @@ from django.db import models
 class Topic(models.Model):
     topic_name=models.CharField(max_length=30,unique=True, blank=False)
 
+    def __str__(self):
+        return self.topic_name
+
 class Webpage(models.Model):
     topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
     name=models.CharField(max_length=100,unique=True,blank=False)
     url=models.URLField(max_length=100,unique=True,blank=False)
 
-class AccessDetails(models.Model):
+    def __str__(self):
+        return self.name
+        
+class AccessDetail(models.Model):
     webpage=models.ForeignKey(Topic,on_delete=models.CASCADE)
     datetime=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.datetime).split(" ")[0]
+        
+
